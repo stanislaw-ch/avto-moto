e69db9c2524ad944f0883e64fcc4a5f7a611c5f9
Stanislaw Chirko <post.chirko@gmail.com>
2020-07-21 20:35:56 +0200

    добавляет завершение верстки с графикой проекта, оптимизацию векторной и растровой графики

--- gulpfile.js
+++ gulpfile.js
@@ -61,13 +61,13 @@ gulp.task("images", function() {
 });
 
 gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
+  return gulp.src('source/img/slide*.{png,jpg}')
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+  return gulp.src('source/img/{icon-*,htmlacademy*}.svg')
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite_auto.svg"))
     .pipe(gulp.dest("build/img"));
@@ -97,5 +97,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "webp", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
