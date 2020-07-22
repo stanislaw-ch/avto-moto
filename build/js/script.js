e9a74d0a2cbe4c1212de03631424599475efb6c3
Stanislaw Chirko <post.chirko@gmail.com>
2020-07-22 17:13:18 +0200

    добавляет реализацию слайдера

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,103 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+var slider = document.querySelector('.slider');
+var sliderPuctures = slider.querySelectorAll('.slider__slide');
+var sliderButtonLeft = slider.querySelector('.slider__button--left');
+var sliderButtonRight = slider.querySelector('.slider__button--right');
+var sliderButtons = slider.querySelectorAll('.slider__button');
+var sliderThumbs = slider.querySelectorAll('.slider__item');
+
+var position = {
+  getMin: 0,
+  getMax: sliderThumbs.length - 1
+};
+
+var sliderPosition = 0;
+
+var sliderTransform = function (direction) {
+  if (direction === 'right') {
+    if (sliderPosition >= position.getMax) {
+      return;
+    }
+    if (sliderButtonLeft.classList.contains('slider__button--disabled')) {
+      sliderButtonLeft.classList.remove('slider__button--disabled');
+    }
+    if (sliderPosition >= position.getMin) {
+      sliderPosition++;
+      sliderThumbs[sliderPosition - 1].classList.remove('slider__item--current');
+      sliderPuctures[sliderPosition - 1].classList.add('slider__slide--hidden');
+      sliderThumbs[sliderPosition].classList.add('slider__item--current');
+      sliderPuctures[sliderPosition].classList.remove('slider__slide--hidden');
+
+    }
+    if (sliderPosition === position.getMax) {
+      sliderButtonRight.classList.add('slider__button--disabled');
+    }
   }
-});
+  if (direction === 'left') {
+    if (sliderPosition === position.getMin) {
+      return;
+    }
+    if (sliderButtonRight.classList.contains('slider__button--disabled')) {
+      sliderButtonRight.classList.remove('slider__button--disabled');
+    }
+    if (sliderPosition <= position.getMax) {
+      sliderPosition--;
+      sliderThumbs[sliderPosition + 1].classList.remove('slider__item--current');
+      sliderPuctures[sliderPosition + 1].classList.add('slider__slide--hidden');
+      sliderThumbs[sliderPosition].classList.add('slider__item--current');
+      sliderPuctures[sliderPosition].classList.remove('slider__slide--hidden');
+    }
+    if (sliderPosition === position.getMin) {
+      sliderButtonLeft.classList.add('slider__button--disabled');
+    }
+  }
+};
+
+var onButtonClick = function (evt) {
+  if (evt.target.classList.contains('slider__button')) {
+    evt.preventDefault();
+    var direction = evt.target.classList.contains('slider__button--right') ? 'right' : 'left';
+    sliderTransform(direction);
+  }
+};
+
+// var onTumbsClick = function (evt) {
+// if (!evt.target.classList.contains('slider__item--current')) {
+// evt.preventDefault();
+// console.log(sliderPosition);
+
+// for (i = 0; i < ) {
+//     sliderPuctures[item].classList.remove('slider__slide--hidden');
+//   }
+// }
+// };
+
+var sliderButtonListener = function () {
+  sliderButtons.forEach(function (item) {
+    item.addEventListener('click', onButtonClick);
+  });
+};
+
+// var sliderTumbsListener = function () {
+//   sliderThumbs.forEach(function (item) {
+//     item.addEventListener('click', onTumbsClick);
+//   });
+// };
+
+sliderButtonListener();
+// sliderTumbsListener();
