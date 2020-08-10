ddc576fbb2fd1eaf3c3678de0e74893af314de65
Stanislaw Chirko <post.chirko@gmail.com>
2020-08-10 21:08:18 +0200

    добавляет промежуточную версиию адаптивной верстки

--- source/js/main.js
+++ source/js/main.js
@@ -37,6 +37,21 @@
 
   var reviewArr = [];
 
+  var pageHeader = document.querySelector('.page-header');
+  var mainNav = pageHeader.querySelector('.main-nav');
+
+  mainNav.classList.remove('main-nav--nojs');
+
+  mainNav.addEventListener('click', function () {
+    if (mainNav.classList.contains('main-nav--closed')) {
+      mainNav.classList.remove('main-nav--closed');
+      mainNav.classList.add('main-nav--opened');
+    } else {
+      mainNav.classList.add('main-nav--closed');
+      mainNav.classList.remove('main-nav--opened');
+    }
+  });
+
   var SliderPosition = {
     GET_MIN: 0,
     GET_MAX: sliderThumbs.length - 1
